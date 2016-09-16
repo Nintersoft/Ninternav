45a91a05acfb730df8b6590f92f4a4c4db7e4182
Mauro Mascarenhas de Araújo <mauro.mascarenhas@nintersoft.ml>
2016-09-16 01:41:57 -0300

    Suporte a abas | Tabs support
    Adicionamos suporte a navegação em abas, porém só é possível abrí-las
    (mas não fechá-las).
    
    EN
    
    We have added tabbed navigation support, but it is only possible to open
    them (for a while you cannot close them).

--- app/src/main/java/com/nintersoft/ninternav/AtividadePrincipal.java
+++ app/src/main/java/com/nintersoft/ninternav/AtividadePrincipal.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -12,56 +13,102 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.AbsoluteLayout;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AtividadePrincipal extends AppCompatActivity {
 
-    private Toolbar toolbar;
     private EditText enderecoDigitado;
-    private WebView navegador;
+    private WebView[] navegador = new WebView[1];
     private Button go;
+    FloatingActionButton abas;
+    private ListView listaAbas;
+    private static int abaAtual = 0;
     public static String erro =
             "<html><head>"+
             "<title>Erro | Ninternav</title>"+
-            "</head><body></br></br><center>Algo deu errado enqanto tentavamos ler a pagina."+
+            "</head><body></br></br><center>Algo deu errado enquanto tentavamos ler a pagina."+
             "</center></body></html>";
+    public static String htmlNovaAba =
+            "<html><head>"+
+                    "<title>Nova aba | Ninternav</title>"+
+                    "</head><body></br></br><center>Esta e a pagina de nova aba. faca o que quiser aqui"+
+                    "</center></body></html>";
+    private ArrayAdapter<String> adaptador;
+    private ArrayList<String> listaDeTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_atividade_principal);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        abas = (FloatingActionButton) findViewById(R.id.botaoAbas);
+        abas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+                navegador[abaAtual].setVisibility(View.GONE);
+                listaAbas.setVisibility(View.VISIBLE);
             }
-        });*/
+        });
 
         enderecoDigitado = (EditText) findViewById(R.id.editText);
+
+        navegador[abaAtual] = (WebView) findViewById(R.id.webView);
+        defineConfigAba(navegador[abaAtual]);
+        navegador[abaAtual].loadDataWithBaseURL("ninterbt:nova_aba", htmlNovaAba, "text/html", null, "ninterbt:nova_aba");
+
         go = (Button) findViewById(R.id.go);
-        navegador = (WebView) findViewById(R.id.webView);
-        navegador.setWebViewClient(new meuNavegador());
-        navegador.getSettings().setLoadsImagesAutomatically(true);
-        navegador.getSettings().setJavaScriptEnabled(true);
-        navegador.getSettings().setBuiltInZoomControls(true);
-        navegador.getSettings().setDisplayZoomControls(false);
-        navegador.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
 
-        enderecoDigitado.setOnClickListener(new View.OnClickListener() {
+        listaAbas = new ListView(this);
+        listaAbas.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT));
+        listaAbas.setVisibility(View.GONE);
+        RelativeLayout pai = (RelativeLayout) findViewById(R.id.relativeLayout);
+        pai.addView(listaAbas);
+
+        listaDeTexto = new ArrayList<>();
+        listaDeTexto.add(navegador[abaAtual].getTitle());
+        adaptador = new ArrayAdapter<>(getApplicationContext(), R.layout.list_view_item_ns, listaDeTexto);
+        listaAbas.setAdapter(adaptador);
+        listaAbas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                if (enderecoDigitado.isInEditMode()) return;
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                listaAbas.setVisibility(View.GONE);
+                abaAtual = i;
+                navegador[abaAtual].setVisibility(View.VISIBLE);
+            }
+        });
 
-                if (!navegador.getUrl().isEmpty()) enderecoDigitado.setText(navegador.getUrl());
+        enderecoDigitado.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (!b){
+                    if (!enderecoDigitado.getText().toString().equals("ninternav:nova_aba"))
+                        enderecoDigitado.setText(Uri.parse(navegador[abaAtual].getUrl()).getHost());
+                    go.setVisibility(View.GONE);
+                    abas.setVisibility(View.GONE);
+                }
+                else{
+                    enderecoDigitado.setText(navegador[abaAtual].getUrl());
+                    go.setVisibility(View.VISIBLE);
+                    abas.setVisibility(View.VISIBLE);
+                }
             }
         });
 
@@ -72,9 +119,16 @@ public class AtividadePrincipal extends AppCompatActivity {
                         (i == KeyEvent.KEYCODE_ENTER || i == KeyEvent.KEYCODE_NAVIGATE_NEXT)
                         && keyEvent.getAction() == KeyEvent.ACTION_DOWN){
                     String url = enderecoDigitado.getText().toString();
-                    if (!url.matches("^\\w+?://.*"))
-                        url = "http://"+url;
-                    navegador.loadUrl(url);
+                    switch (url){
+                        case "ninternav:nova_aba":
+                            navegador[abaAtual].loadDataWithBaseURL("ninterbt:nova_aba", htmlNovaAba, "text/html", null, "ninterbt:nova_aba");
+                            break;
+                        default:
+                            if (!url.matches("^\\w+?://.*"))
+                                url = "http://"+url;
+                            navegador[abaAtual].loadUrl(url);
+                            break;
+                    }
                     enderecoDigitado.setText(url);
 
                     view.clearFocus();
@@ -85,7 +139,7 @@ public class AtividadePrincipal extends AppCompatActivity {
                 }
                 else if (enderecoDigitado.getText().toString().isEmpty() &&
                         (i == KeyEvent.KEYCODE_ENTER || i == KeyEvent.KEYCODE_NAVIGATE_NEXT)) {
-                    enderecoDigitado.setText(navegador.getUrl());
+                    enderecoDigitado.setText(navegador[abaAtual].getUrl());
 
                     view.clearFocus();
                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
@@ -98,22 +152,26 @@ public class AtividadePrincipal extends AppCompatActivity {
         go.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!enderecoDigitado.getText().toString().isEmpty()){
+                if (!enderecoDigitado.getText().toString().isEmpty())
+                {
                     String url = enderecoDigitado.getText().toString();
-
-                    if (!url.matches("^\\w+?://.*"))
-                        url = "http://" + url;
-                    navegador.loadUrl(url);
+                    switch (url){
+                        case "ninternav:nova_aba":
+                            navegador[abaAtual].loadDataWithBaseURL("ninterbt:nova_aba", htmlNovaAba, "text/html", null, "ninterbt:nova_aba");
+                            break;
+                        default:
+                            if (!url.matches("^\\w+?://.*"))
+                                url = "http://"+url;
+                            navegador[abaAtual].loadUrl(url);
+                            break;
+                    }
                     enderecoDigitado.setText(url);
 
-                    view.clearFocus();
                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                     if (imm.isActive()) imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
                 else {
-                    enderecoDigitado.setText(navegador.getUrl());
-
-                    view.clearFocus();
+                    if (enderecoDigitado.isFocused()) enderecoDigitado.clearFocus();
                     InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
                     if (imm.isActive()) imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
                 }
@@ -121,31 +179,6 @@ public class AtividadePrincipal extends AppCompatActivity {
         });
 
 
-    }
-
-    private class meuNavegador extends WebViewClient {
-        @SuppressWarnings("deprecation")
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url){
-            enderecoDigitado.setText(Uri.parse(url).getHost());
-        }
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon){
-            enderecoDigitado.setText(url);
-        }
-
-        @SuppressWarnings("deprecation")
-        @Override
-        public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
-            navegador.loadData(erro, "text/html", null);
-        }
     }
 
     @Override
@@ -167,6 +200,108 @@ public class AtividadePrincipal extends AppCompatActivity {
             return true;
         }
 
+        if (id == R.id.action_newtab){
+            navegador[abaAtual].setVisibility(View.GONE);
+
+            WebView[] temp = new WebView[navegador.length + 1];
+            for (int i = 0; i < navegador.length; ++i)
+                temp[i] = navegador[i];
+
+            navegador = temp;
+            abaAtual = navegador.length - 1;
+            navegador[abaAtual] = criarAba();
+
+            RelativeLayout pai = (RelativeLayout) findViewById(R.id.relativeLayout);
+            pai.addView(navegador[abaAtual]);
+            navegador[abaAtual].setVisibility(View.VISIBLE);
+
+            navegador[abaAtual].loadDataWithBaseURL("ninterbt:nova_aba", htmlNovaAba, "text/html", null, "ninterbt:nova_aba");
+
+            enderecoDigitado.setText(navegador[abaAtual].getTitle());
+
+            return true;
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private WebView criarAba (){
+        WebView novaAba= new WebView(this);
+        novaAba.setWebViewClient(new WebViewClient(){
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url){
+                enderecoDigitado.setText(Uri.parse(url).getHost());
+                listaDeTexto.set(abaAtual, view.getTitle());
+                adaptador.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon){
+                if (!enderecoDigitado.getText().toString().equals("ninternav:nova_aba"))
+                    enderecoDigitado.setText(url);
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+                view.loadDataWithBaseURL("ninternav:erro",erro, "text/html", null, "ninternav:erro");
+                enderecoDigitado.setText(view.getUrl());
+            }
+        });
+        novaAba.getSettings().setLoadsImagesAutomatically(true);
+        novaAba.getSettings().setJavaScriptEnabled(true);
+        novaAba.getSettings().setBuiltInZoomControls(true);
+        novaAba.getSettings().setDisplayZoomControls(false);
+        novaAba.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+
+        novaAba.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                                                            ViewGroup.LayoutParams.MATCH_PARENT));
+
+        listaDeTexto.add("Nova Aba | Ninternav");
+        adaptador.notifyDataSetChanged();
+
+        return novaAba;
+    }
+
+    private void defineConfigAba(WebView nav){
+        nav.setWebViewClient(new WebViewClient(){
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url){
+                enderecoDigitado.setText(Uri.parse(url).getHost());
+                listaDeTexto.set(abaAtual, view.getTitle());
+                adaptador.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon){
+                //if (!enderecoDigitado.getText().toString().equals("ninternav:nova_aba"))
+                enderecoDigitado.setText(url);
+            }
+
+            @SuppressWarnings("deprecation")
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl){
+                view.loadDataWithBaseURL("ninternav:erro",erro, "text/html", null, "ninternav:erro");
+                enderecoDigitado.setText(view.getUrl());
+            }
+        });
+        nav.getSettings().setLoadsImagesAutomatically(true);
+        nav.getSettings().setJavaScriptEnabled(true);
+        nav.getSettings().setBuiltInZoomControls(true);
+        nav.getSettings().setDisplayZoomControls(false);
+        nav.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
     }
 }
